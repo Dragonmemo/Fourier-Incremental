6de5f62866e6bf5a2e2edcb79fb12d4fe7fa70a1
Dragonmemo <lucasmaub@free.fr>
2020-06-20 16:28:35 +0200

    Possible typo

--- script.js
+++ script.js
@@ -125,7 +125,7 @@ function IncrementCMn(n) {
 function PrestigeNegative(){
 	if (x<1e11){return};
 	y+=math.floor(math.log10(x)-10);
-	if (Achieves[62]=0) {
+	if (Achieves[62]==0) {
 		Achieves[62]++;
 		document.getElementById("CM1Tab").removeAttribute("hidden");		
 		};

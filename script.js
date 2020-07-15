f1b5e368ac23db6bbfe01a73198d279969713eaa
Dragonmemo <lucasmaub@free.fr>
2020-07-15 19:55:26 +0200

    Update script.js

--- script.js
+++ script.js
@@ -509,6 +509,40 @@ function HReset(){
 	GetSkillTree()
 }
 //loading all the stuff...
+function copyStringToClipboard(str) {
+  var el = document.createElement("textarea");
+  el.value = str;
+  el.setAttribute("readonly", "");
+  el.style = {
+    position: "absolute",
+    left: "-9999px"
+  };
+  document.body.appendChild(el);
+  copyToClipboard(el);
+  document.body.removeChild(el);
+  alert("Copied to clipboard");
+}
+
+function copyToClipboard(el) {
+  el = typeof el === "string" ? document.querySelector(el) : el;
+  if (navigator.userAgent.match(/ipad|ipod|iphone/i)) {
+    var editable = el.contentEditable;
+    var readOnly = el.readOnly;
+    el.contentEditable = true;
+    el.readOnly = true;
+    var range = document.createRange();
+    range.selectNodeContents(el);
+    var selection = window.getSelection();
+    selection.removeAllRanges();
+    selection.addRange(range);
+    el.setSelectionRange(0, 999999);
+    el.contentEditable = editable;
+    el.readOnly = readOnly;
+  } else {
+    el.select();
+  }
+  document.execCommand("copy");
+}
 function Export(){
 	copyStringToClipboard(btoa(JSON.stringify(localStorage)));
 }

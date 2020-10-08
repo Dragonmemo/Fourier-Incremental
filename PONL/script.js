dd7783cf2b16631ea57758928b8c93f9d93469dd
Dragonmemo <67173828+Dragonmemo@users.noreply.github.com>
2020-10-08 19:56:04 +0200

    Add files via upload

--- PONL/script.js
+++ PONL/script.js
@@ -1,8 +1,6 @@
 // client-side js, loaded by index.html
 // run by the browser each time the page is loaded
 
-// define variables that reference elements on our page
-//Decimal.toExpPos= 2;
 var LISTER;
 var Onglet=1;
 var SETTER = false;

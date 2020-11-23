fb2c4e1adf29603a965194c8a62840eb00416a04
Dragonmemo <lucasmaub@free.fr>
2020-11-23 18:04:50 +0100

    new game launch

---
+++ Spectral-Book/script.js
@@ -0,0 +1,110 @@
+
+function myFunction() {
+  var canvas = document.getElementById("myCanvas");
+  var ctx = canvas.getContext("2d");
+  ctx.clearRect(0, 0, 500, 500);
+  ctx.strokeStyle="#000099";
+  ctx.moveTo(250, 0);
+  ctx.lineTo(250, 500);
+  ctx.stroke(); 
+  ctx.moveTo(0, 250);
+  ctx.lineTo(500, 250);
+  ctx.stroke();
+  ctx.strokeStyle="#BBBBBB";
+  ctx.beginPath();
+  ctx.closePath();
+};
+
+var tickpart=0;
+var tickpart2=0;
+var mainGameLoop = window.setInterval(function() { // runs the loop
+	tickpart += 50;
+	tickpart2+=50;
+	if (tickpart>=tickspeed) {
+		tickpart -= tickspeed;
+		loop();
+	}
+	if (tickpart2>=10000){
+		tickpart2-=10000
+		//if (document.getElementById("Autosave").checked == true){save();}
+	}
+}, 50);
+
+function loop() { // production
+  myFunction();
+}
+/*
+if(!(localStorage.lastTick)){localStorage.lastTick=Date.now();}
+function save() { 
+  localStorage.setItem('Max',MAX);
+  localStorage.setItem("MCur",x);
+  localStorage.setItem("PCur",y);
+  localStorage.setItem("Zero",Zero);
+  localStorage.setItem("Coords",coords);
+  localStorage.setItem("PCN",positives);
+  localStorage.setItem("NCN",negatives);
+  localStorage.setItem("PBC",pbaseCost);
+  localStorage.setItem("NPOW",NPOW);
+  localStorage.setItem("NFIC",FicNeg);  
+  localStorage.setItem("Other",OtherQuantity);
+  localStorage.setItem("R",R);
+  localStorage.setItem("Achieves",Achieves);
+  localStorage.setItem("Skills",Skills);
+  localStorage.setItem("lastTick",Date.now());
+  localStorage.setItem("AutoBuy",document.getElementById("AUTOBUY").checked)
+} 
+//loading all the stuff...
+function copyStringToClipboard(str) {
+  var el = document.createElement("textarea");
+  el.value = str;
+  el.setAttribute("readonly", "");
+  el.style = {
+    position: "absolute",
+    left: "-9999px"
+  };
+  document.body.appendChild(el);
+  copyToClipboard(el);
+  document.body.removeChild(el);
+  alert("Copied to clipboard");
+}
+
+function copyToClipboard(el) {
+  el = typeof el === "string" ? document.querySelector(el) : el;
+  if (navigator.userAgent.match(/ipad|ipod|iphone/i)) {
+    var editable = el.contentEditable;
+    var readOnly = el.readOnly;
+    el.contentEditable = true;
+    el.readOnly = true;
+    var range = document.createRange();
+    range.selectNodeContents(el);
+    var selection = window.getSelection();
+    selection.removeAllRanges();
+    selection.addRange(range);
+    el.setSelectionRange(0, 999999);
+    el.contentEditable = editable;
+    el.readOnly = readOnly;
+  } else {
+    el.select();
+  }
+  document.execCommand("copy");
+}
+function Export(){
+	copyStringToClipboard(btoa(JSON.stringify(localStorage)));
+}
+function Import(){
+  let loadgame = "";
+  loadgame = prompt("Paste in your save WARNING: WILL OVERWRITE YOUR CURRENT SAVE");
+  if (loadgame=="Fermat" && Achieves[68]==0){
+	  Achieves[68]++;
+	SOUND.pause();SOUND.play();
+	document.getElementById("A7C9").setAttribute("style","background-color: #5B5;");
+	document.getElementById("A7C9").removeAttribute("hidden");
+  }
+  else {
+	  if (loadgame !="" ) {
+	  
+  }}
+}
+if(localStorage.MCur) {
+  
+}*/
